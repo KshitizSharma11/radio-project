5b03b6fd6676ab7dc52a3d722276ac6240390ff1
Isha382 <ishachoudhary382@gmail.com>
2022-09-30 10:12:19 +0530

    Added image and text overlay

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 
 import './App.css';
+import "./components/style.css";
 import Player from './components/player';
 
 function App() {
   return (
-    <div className="App  h-max">
-      <div className=" bg-red-200">
-      <h1 className="text-9xl font-size font-bold font-serif text-white text-center pt-20">Welcome </h1>
-      <h1 className="text-9xl font-bold font-serif text-white text-center pt-5">To </h1>
-      <h1 className="text-9xl font-bold font-serif text-white text-center pt-5 pb-10">Radio-CU</h1>
-      <marquee className="text-4xl font-bold font-serif text-white text-center mb-9 ">Made And Designed By Cuians</marquee>
-      </div>
-      <div>
+    <div className="App  h-max" id='main' class="example">
+     
+     
+      <h1 className="text-9xl font-size  font-serif text-red text-center pt-20">Welcome </h1>
+      <h1 className="text-9xl  font-serif text-red text-center pt-5">To </h1>
+      <h1 className="text-9xl  font-serif text-red text-center pt-5 pb-10">Radio-CU</h1>
+      <marquee className="text-4xl  font-serif text- text-center mb-9 ">Made And Designed By Cuians</marquee>
+    
+      
       <Player></Player>
-      </div>
+      
     </div>
+   
   );
 }
 
